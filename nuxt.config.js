59cec3ac3b67d67622c2e76ac66029234c6aefe7
Davies Okpeta <davies19okpeta@gmail.com>
2023-01-20 19:23:17 +0100

    git build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 export default {
+  target: 'static',
+  router: {
+    base: '/eagle-eye-ecommerce/'
+  },
   ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
